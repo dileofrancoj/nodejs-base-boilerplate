9de0701d4542bd689d7c3cc3d83c96a12d6b8985
Franco Di Leo <dileo.francoj@gmail.com>
2022-04-25 20:19:02 -0300

    feat: add prettier

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
 /* eslint-disable no-undef */
-if (process.env.NODE_ENV === "local") {
-  require("dotenv").config({ path: ".env.local" });
+if (process.env.NODE_ENV === 'local') {
+  require('dotenv').config({ path: '.env.local' })
 }
--- src/index.js
+++ src/index.js
@@ -1,28 +1,26 @@
-import express from "express";
-import morgan from "morgan";
-import cors from "cors";
+import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
 
 // eslint-disable-next-line no-unused-vars
-import * as _ from "./config";
+import * as _ from './config'
 
-const app = express();
+const app = express()
 
 // eslint-disable-next-line no-undef
-const port = process.env.PORT || 8005;
-const host = "0.0.0.0";
+const port = process.env.PORT || 8005
+const host = '0.0.0.0'
 
-app.use(cors());
-app.use(express.json());
-app.use(morgan("combined"));
+app.use(cors())
+app.use(express.json())
+app.use(morgan('combined'))
 
-app.use("/api/ping", (_, res) => {
-  res.json({ message: "Pong" });
-});
+app.use('/api/ping', (_, res) => {
+  res.json({ message: 'Pong' })
+})
 
-app.listen(port, host, () =>
-  console.info(`Nodejs App Listening on port: ${port}`)
-);
+app.listen(port, host, () => console.info(`App Listening on port: ${port}`))
 
 app.use((_, res) => {
-  res.status(404).json({ message: "Sorry can't find that!" });
-});
+  res.status(404).json({ message: "Sorry can't find that!" })
+})
