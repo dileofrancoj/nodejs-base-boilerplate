dfb13fddd4ec54acc90841e56d1766eccf49fc66
Franco Di Leo <dileo.francoj@gmail.com>
2022-04-25 19:04:01 -0300

    feat: add eslint config

--- src/config/index.js
+++ src/config/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 if (process.env.NODE_ENV === "local") {
   require("dotenv").config({ path: ".env.local" });
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import morgan from "morgan";
-import cors from 'cors'
+import cors from "cors";
 
+// eslint-disable-next-line no-unused-vars
 import * as _ from "./config";
 
 const app = express();
 
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 8005;
 const host = "0.0.0.0";
 
@@ -20,10 +22,6 @@ app.use("/api/ping", (_, res) => {
 app.listen(port, host, () =>
   console.info(`Nodejs App Listening on port: ${port}`)
 );
-
-app.use((error, req, res, next) => {
-  return errorHandler(error, res);
-});
 
 app.use((_, res) => {
   res.status(404).json({ message: "Sorry can't find that!" });
